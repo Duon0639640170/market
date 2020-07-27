9cb96bf986a841ad514288d10904722701fba27d
[joy] <[don24185@gmail.com]>
2020-07-27 22:16:24 +0700

    - Test Commit

--- src/main/java/com/it/entity/OrderEntity.java
+++ src/main/java/com/it/entity/OrderEntity.java
@@ -10,6 +10,7 @@ import javax.persistence.Table;
 @Entity
 @Table(name = "tb_order")
 public class OrderEntity {
+	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "order_id")
